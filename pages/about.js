3605d3f4c5927d3321ad2b582183690989e9c615
Trevor Jones <degengenesis@protonmail.com>
2022-12-01 13:29:49 -0800

    Add: boilerplate

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+// about page
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
---
+++ pages/signup.js
@@ -0,0 +1,14 @@
+// sign up page
+
+import Link from "next/link";
+
+export default function SignUp() {
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
