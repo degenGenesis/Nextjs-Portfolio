7172ab79f8028729b2709cc4d83fb8479f33f194
Trevor Jones <degengenesis@protonmail.com>
2022-12-10 23:47:47 -0800

    Add: implement initial bio paragraph

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,19 @@
 // about page
 import Link from "next/link";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import styles from '../styles/Home.module.css'
 
 export default function About() {
   return (
     <>
+      <Header />
       <h1>About</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <Link href="/">Back to home</Link>
+      <p>
+        As a developer, I have a passion for creating intuitive and user-friendly experiences. I am always seeking opportunities to learn and improve my skills and am excited to bring my expertise to a new team. Thank you for considering me for your project.
+      </p>
+      <Footer />
     </>
   );
 }
