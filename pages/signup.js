ad76574d345ffd6d590b8f06cd42f07157db12d2
Trevor Jones <degengenesis@protonmail.com>
2022-12-11 23:42:42 -0800

    Add: implement <Header /> & <Footer /> components

--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,19 @@
 // sign up page
 
 import Link from "next/link";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import styles from "../styles/Home.module.css";
 
 export default function SignUp() {
   return (
     <>
+      <Header />
       <h1>Sign Up</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <Footer />
     </>
   );
 }
