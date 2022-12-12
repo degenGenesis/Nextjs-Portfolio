6a7dee3d1a4dab193a03db7f3692820ac28610c1
Trevor Jones <degengenesis@protonmail.com>
2022-12-11 23:41:29 -0800

    Add: implement <Header /> & <Footer /> components

--- pages/projects/projectsHome.js
+++ pages/projects/projectsHome.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import styles from '../../styles/Home.module.css';
 
 export default function ProjectsHome() {
   return (
     <>
+      <Header />
       <h1>Here are all my projects.</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <Footer />
     </>
   );
 }
